651f20189d240b6e7156107a704649874712ec1a
letsspeak <letsinfinite@gmail.com>
2017-12-30 22:44:54 +0900

    fix Package.swift for adding Linux dependencies

--- Package.swift
+++ Package.swift
@@ -2,20 +2,40 @@
 
 import PackageDescription
 
+var packageDependencies: [Package.Dependency] = [
+    .package(url: "https://github.com/vapor/vapor.git", .upToNextMajor(from: "2.1.0")),
+    .package(url: "https://github.com/vapor/leaf-provider.git", from: "1.0.0"),
+    .package(url: "https://github.com/vapor/fluent-provider.git", .upToNextMajor(from: "1.2.0")),
+    .package(url: "https://github.com/DanToml/Jay.git", from: "1.0.0"),
+    .package(url: "https://github.com/remko/swift-duktape.git", .upToNextMinor(from: "0.2.0")),
+]
+
+var appDependencies: [Target.Dependency] = [
+    "Vapor",
+    "LeafProvider",
+    "FluentProvider",
+    "Jay"
+]
+
+#if os(Linux)
+let duktape: Package.Dependency =
+    .package(url: "https://github.com/remko/swift-duktape.git", .upToNextMinor(from: "0.2.0"))
+packageDependencies.append(duktape)
+let duktapePackage: Target.Dependency =
+    .byName(name: "swift-duktape")
+appDependencies.append(duktapePackage)
+#endif
+
 let package = Package(
     name: "Stock",
     products: [
         .library(name: "App", targets: ["App"]),
         .executable(name: "stock", targets: ["Run"])
     ],
-    dependencies: [
-        .package(url: "https://github.com/vapor/vapor.git", .upToNextMajor(from: "2.1.0")),
-        .package(url: "https://github.com/vapor/leaf-provider.git", from: "1.0.0"),
-        .package(url: "https://github.com/vapor/fluent-provider.git", .upToNextMajor(from: "1.2.0")),
-        .package(url: "https://github.com/DanToml/Jay.git", from: "1.0.0"),
-    ],
+    dependencies: packageDependencies,
     targets: [
-        .target(name: "App", dependencies: ["Vapor", "LeafProvider", "FluentProvider", "Jay"],
+        .target(name: "App",
+                dependencies: appDependencies,
                 exclude: [
                     "Config",
                     "Public",
@@ -25,8 +45,3 @@ let package = Package(
         .testTarget(name: "AppTests", dependencies: ["App", "Testing"])
     ]
 )
-
-#if os(Linux)
-let duktape = Package(url: "https://github.com/remko/swift-duktape.git", .upToNextMinor(from: "0.2.0"))
-package.dependencies.append(duktape)
-#endif
